125932509817c25a863d11672b29d8bbcf832d16
Luciano <lof.fagundes@gmail.com>
2025-06-02 11:19:34 -0300

    faker dados

--- src/main/java/dev/codex/factory/dataFactory/DataFactory.java
+++ src/main/java/dev/codex/factory/dataFactory/DataFactory.java
@@ -1,13 +1,18 @@
 package dev.codex.factory.dataFactory;
 
+import com.github.javafaker.Faker;
+
 import dev.codex.dto.dataDTO.DataDTO;
 
+import java.util.Locale;
+
 public class DataFactory {
+
     public static DataDTO dataDto() {
         return new DataDTO(
-                "Ricardo",
-                "Costa",
-                "91110000"
+                Faker.instance().name().firstName(),
+                Faker.instance().name().lastName(),
+                Faker.instance(Locale.forLanguageTag("pt-BR")).address().zipCode()
         );
     }
 }
